63fe40d2b3c4b8693b5def1e981777de47adff15
angelvenchev <angelvenchev@yahoo.com>
2015-03-08 00:24:19 +0100

    Convert tuple to better named class

--- app/src/main/java/org/intracode/sortvisualizer/sortalgorithms/SortAlgorithm.java
+++ app/src/main/java/org/intracode/sortvisualizer/sortalgorithms/SortAlgorithm.java
@@ -6,9 +6,6 @@ import java.util.List;
  * Created by angelvenchev on 3/7/2015.
  */
 public interface SortAlgorithm {
-
-
-    public List<Tuple<int[], boolean[]>> SortSteps(Integer[] numbers);
-
+    public List<SortStep> SortSteps(Integer[] numbers);
 
 }
--- app/src/main/java/org/intracode/sortvisualizer/sortalgorithms/SortStep.java
+++ app/src/main/java/org/intracode/sortvisualizer/sortalgorithms/SortStep.java
@@ -3,7 +3,7 @@ package org.intracode.sortvisualizer.sortalgorithms;
 /**
  * Created by angelvenchev on 3/7/2015.
  */
-public class Tuple<T,K> {
-    public T first;
-    public K second;
+public class SortStep {
+    public int[] numbers;
+    public boolean[] numberHighlighters;
 }
